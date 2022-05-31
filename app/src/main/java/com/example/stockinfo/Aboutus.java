0424e772066b9393df09c70f797497bc706cc3ec
anikurhade <anikurhade2002@gmail.com>
2022-05-31 09:32:32 +0530

    Created About Us Activity

---
+++ app/src/main/java/com/example/stockinfo/Aboutus.java
@@ -0,0 +1,23 @@
+package com.example.stockinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Aboutus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+    public void GotoDashboard(View V)
+    {
+        startActivity(new Intent(getApplicationContext(),Dashboard.class));
+    }
+}
