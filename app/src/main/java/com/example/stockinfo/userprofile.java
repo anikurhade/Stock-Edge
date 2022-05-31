1700f8154df95e40398bff5f733e6a3065afefb3
anikurhade <anikurhade2002@gmail.com>
2022-05-31 11:02:35 +0530

    Updated User Profile With Connecting It To Firebase For Getting The Data

--- app/src/main/java/com/example/stockinfo/userprofile.java
+++ app/src/main/java/com/example/stockinfo/userprofile.java
@@ -1,15 +1,25 @@
 package com.example.stockinfo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class userprofile extends AppCompatActivity {
-
+    TextView name,phone,email;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fstore;
+    String Userid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +27,25 @@ public class userprofile extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+         name=findViewById(R.id.disname);
+         phone=findViewById(R.id.disphone);
+         email=findViewById(R.id.disemail);
+
+         fAuth=FirebaseAuth.getInstance();
+         fstore=FirebaseFirestore.getInstance();
+         Userid=fAuth.getCurrentUser().getUid();
+
+        DocumentReference df=fstore.collection("users").document(Userid);
+        df.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                phone.setText(value.getString("phone"));
+                name.setText(value.getString("fName"));
+                email.setText(value.getString("email"));
+
+            }
+        });
     }
     public void GotoDashboard(View V)
     {
