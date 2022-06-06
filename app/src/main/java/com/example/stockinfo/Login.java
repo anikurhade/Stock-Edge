33d775cc149b67a27ae29f5ca028d8dde3616738
anikurhade <anikurhade2002@gmail.com>
2022-06-07 00:02:07 +0530

    Updating Login Activity With Forget Password

--- app/src/main/java/com/example/stockinfo/Login.java
+++ app/src/main/java/com/example/stockinfo/Login.java
@@ -1,8 +1,10 @@
 package com.example.stockinfo;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,15 +12,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     EditText gmail, password;
+    TextView forgetpassword;
     Button logn;
 
     ProgressBar pblogn;
@@ -35,8 +41,10 @@ public class Login extends AppCompatActivity {
         gmail = findViewById(R.id.mail);
         password = findViewById(R.id.passwordlgn);
         logn = findViewById(R.id.lgn);
+        forgetpassword=findViewById(R.id.forgetpass);
         fAuth = FirebaseAuth.getInstance();
         pblogn = findViewById(R.id.pblgn);
+
     logn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -77,9 +85,16 @@ public class Login extends AppCompatActivity {
             });
         }
     });
-
+forgetpassword.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(getApplicationContext(), forgetpassword.class));
+    }
+});
 
     }
+
+
     public void onClickRegirecttoregister (View v)
     {
         Intent i = new Intent(Login.this, Register.class);
